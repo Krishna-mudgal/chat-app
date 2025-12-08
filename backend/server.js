ec7606dfe51985023d189dbd272986318d1d4d5e
Krishna-mudgal <tonystark00127@gmail.com>
2025-12-09 01:21:45 +0530

    fix port issue

--- backend/server.js
+++ backend/server.js
@@ -193,5 +193,5 @@ app.use("/api/messages", auth, messageRouter);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${5000}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
